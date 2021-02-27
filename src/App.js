ee349970c9fd6a03a0e2f7b702ac09f4898b5787
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-27 21:46:48 +0100

    state management - useReducer adeed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import AppStateProvider from './context/appStateProvider';
-import ChannelList from './components/channelList';
-import FetchBar from './components/fetchBar';
-import VideoDetails from './components/videoDetails';
-import VideoList from './components/videoList';
+import React from "react";
+import AppStateProvider from "./context/appStateProvider";
+import ChannelList from "./components/channelList";
+import FetchBar from "./components/fetchBar";
+import VideoDetails from "./components/videoDetails";
+import VideoList from "./components/videoList";
 
 import "./styles/App.css";
 
 const App = () => {
   return (
-  <AppStateProvider>
-<div>
-  <ChannelList/>
-</div>
-<div>
-  <FetchBar/>
-</div>
-<div>
-  <VideoList/>
-</div>
-<div>
-  <VideoDetails/>
-</div>
-  </AppStateProvider>;
-  )}
+    <AppStateProvider>
+      <div>
+        <ChannelList />
+      </div>
+      <div>
+        <FetchBar />
+      </div>
+      <div>
+        <VideoList />
+      </div>
+      <div>
+        <VideoDetails />
+      </div>
+    </AppStateProvider>
+  );
+};
 
 export default App;
